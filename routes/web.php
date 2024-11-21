f5b649db665ff1556d83e0d69800d00ca78b2218
PaoloIncollu <paoloinco@gmail.com>
2024-11-21 16:50:06 +0100

    aggiunta rotta menuItems

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 // Controllers
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\Admin\MainController as AdminMainController;
+use App\Http\Controllers\Admin\MenuItemController as AdminMenuItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +26,7 @@ Route::prefix('admin')
     ->group(function () {
 
     Route::get('/dashboard', [AdminMainController::class, 'dashboard'])->name('dashboard');
+    Route::resource('menu_items', AdminMenuItemController::class);
 
 });
 

a01243523f3eb5d0d54e7f38273592684d35c52d
micheledimassimo <mcldms@outlook.it>
2024-11-21 17:06:42 +0100

    piccole modifiche

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,59 +21,75 @@
 
         <!-- Name -->
         <div class="mt-4">
-            <label for="first-name">
-                Name
-            </label>
-            <input type="text" id="first-name" name="first-name" required minlength="1" maxlength="32">
+            <div>
+                <label for="first-name">
+                    Nome
+                </label>
+            </div>
+            <input type="text" id="first-name" name="first-name" required minlength="1" maxlength="32" placeholder="Inserisci qui il tuo nome..">
         </div>
 
         <div class="mt-4"> 
-            <label for="last-name">
-               Surname
-            </label>
-            <input type="text" id="last-name" name="last-name" required minlength="1" maxlength="32">
+            <div>
+                <label for="last-name">
+                Cognome
+                </label>
+            </div>
+            <input type="text" id="last-name" name="last-name" required minlength="1" maxlength="32" placeholder="Inserisci qui il tuo cognome..">
         </div>
 
         <!-- Partita IVA -->
         <div class="mt-4">
-            <label for="p-iva">
-               Numero Partita IVA
-            </label>
-            <input type="text" id="p-iva" name="p-iva" required minlength="11" maxlength="11">
+            <div>
+                <label for="p-iva">
+                Numero Partita IVA
+                </label>
+            </div>
+            <input type="text" id="p-iva" name="p-iva" required minlength="11" maxlength="11" placeholder="Inserisci qui la partita IVA..">
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
-            <label for="email">
-                Email
-            </label>
-            <input type="email" id="email" name="email" required maxlength="255">
+            <div>
+                <label for="email">
+                    Email
+                </label>
+            </div>
+            <input type="email" id="email" name="email" required maxlength="255" placeholder="Inserisci qui la tua email..">
         </div>
 
         <!-- Password -->
         <div class="mt-4">
+            <div>
             <label for="password">
                 Password
             </label>
-            <input type="password" id="password" name="password" required maxlength="64">
+            </div>
+            <input type="password" id="password" name="password" required maxlength="64" placeholder="Inserisci password..">
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <label for="password_confirmation">
-                Conferma Password
-            </label>
-            <input type="password" id="password_confirmation" name="password_confirmation" required>
+            <div>
+                <label for="password_confirmation">
+                    Conferma Password
+                </label>
+            </div>
+            <input type="password" id="password_confirmation" name="password_confirmation" required placeholder="Conferma password..">
         </div>
 
-        <div>
+        <div class="mt-4">
             <a href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
-            <button type="submit">
+            <button class="mx-2" type="submit">
                 Register
             </button>
+
+            <p>
+                Tutti i campi sono obbligatori
+            </p>
         </div>
     </form>
 @endsection

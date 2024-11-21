945bc414b93a21ecbe945f04ced86c43fb9c9e8d
micheledimassimo <mcldms@outlook.it>
2024-11-21 18:02:21 +0100

    creati model tabella controller e seeder typologies

---
+++ app/Http/Controllers/Admin/TypologyController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Typology;
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
+class TypologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Typology $typology)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Typology $typology)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Typology $typology)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Typology $typology)
+    {
+        //
+    }
+}

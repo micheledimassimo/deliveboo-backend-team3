80febac0a1e703c32f5bb0589151f70fbb7ce330
Eduard Condurache <eduardcondurache96@gmail.com>
2024-11-21 14:59:11 +0100

    Creato seeder per il Restaurants

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -5,6 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Helpers
+
+use Illuminate\Support\Facades\Schema;
+
+// Models
+
+use App\Models\Restaurant;
+use app\Models\User;
+
 class RestaurantSeeder extends Seeder
 {
     /**
@@ -12,6 +21,30 @@ class RestaurantSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Schema::withoutForeignKeyConstraints(function () {
+            Restaurant::truncate();
+        });
+
+        // Da riguardare
+
+        for($i = 0; $i < 10; $i++) {
+            $name = fake()->company();
+            $slug = Restaurant::getUniqueSlug($name);
+            $randomUser = User::inRandomOrder()->first();
+            $restaurant = Restaurant::create([
+                'restaurant_name' => $name,
+                'address' => fake()->address(),
+                'phone_number' => fake()->phoneNumber(),
+                'slug' => $slug,
+                'user_id' => $randomUser->id,
+            ]);
+            $usersIds = [];
+            for($j = 0; $j < rand(0, User::count()); $j++) {
+                $randomUser = User::inRandomOrder()->first();
+                if (!in_array($randomUser->id, $usersIds)) {
+                    $usersIds[] = $randomUser->id;
+                }
+        }
     }
+}
 }

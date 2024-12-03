490bec592ec723d661e4add26a173e33c859c78b
PaoloIncollu <paoloinco@gmail.com>
2024-12-03 16:17:49 +0100

    aggiustato carosello ordini

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
 import './bootstrap';
 
 import '~resources/scss/app.scss';
-import './carouselControls.js';
+
 
 import * as bootstrap from 'bootstrap';
-
+import './carouselControls.js';
 
 import.meta.glob([
     '../img/**'
